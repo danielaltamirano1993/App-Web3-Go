157f27ff931595dd15064dc6a455edb6bf9f91c3
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-11-02 09:34:54 -0500

    Create utils_test.go

---
+++ utils/utils_test.go
@@ -0,0 +1,105 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+func TestToWei(t *testing.T) {
+	ethVal := 0.003
+	u := NewUtils()
+	wei := u.ToWei(ethVal)
+	fmt.Printf("wei %v\n", wei)
+	fmt.Printf("wei hex %v\n", u.ToHex(wei))
+}
+
+func TestFromWei(t *testing.T) {
+	ethVal := 0.00000001
+	u := NewUtils()
+	wei := u.ToWei(ethVal)
+	fmt.Printf("wei %v\n", wei)
+	gwei := u.FromWeiWithUnit(wei, EtherUnitGWei)
+	fmt.Printf("gwei %.4f\n", gwei)
+}
+
+func TestSignMethod(t *testing.T) {
+
+	funcName := "freeMint()"
+	id := crypto.Keccak256([]byte(funcName))[:4]
+	fmt.Printf("id 0x%x\n", id)
+
+	funcName = "mint()"
+	id = crypto.Keccak256([]byte(funcName))[:4]
+	fmt.Printf("id 0x%x\n", id)
+}
+
+func TestSameAddr(t *testing.T) {
+	addr1 := common.HexToAddress("0x0000000000fC95fD88A4c46d9d7984A56289c52A")
+	addr2 := common.HexToAddress("0x0000000000fc95fd88a4c46d9d7984a56289c52a")
+
+	fmt.Printf("addr1 %v, addr2 %v\n", addr1, addr2)
+
+	fmt.Printf("addr1 == addr2 %t\n", addr1 == addr2)
+
+	fmt.Printf("bytes cmp addr1 == addr2 %t\n", bytes.Compare(addr1[:], addr2[:]) == 0)
+}
+
+func BenchmarkTestCompare1(b *testing.B) {
+	addr1 := common.HexToAddress("0x0000000000fC95fD88A4c46d9d7984A56289c52A")
+	addr2 := common.HexToAddress("0x0000000000fc95fd88a4c46d9d7984a56289c52a")
+
+	for i := 0; i < b.N; i++ {
+		if addr1 == addr2 {
+			continue
+		}
+	}
+}
+
+func BenchmarkTestCompare2(b *testing.B) {
+	addr1 := common.HexToAddress("0x0000000000fC95fD88A4c46d9d7984A56289c52A")
+	addr2 := common.HexToAddress("0x0000000000fc95fd88a4c46d9d7984a56289c52a")
+
+	for i := 0; i < b.N; i++ {
+		if bytes.Compare(addr1[:], addr2[:]) == 0 {
+			continue
+		}
+	}
+}
+
+func BenchmarkTestCompare3(b *testing.B) {
+	addr1 := common.HexToAddress("0x0000000000fC95fD88A4c46d9d7984A56289c52A")
+	addr2 := common.HexToAddress("0x0000000000fc95fd88a4c46d9d7984a56289c52a")
+
+	for i := 0; i < b.N; i++ {
+		if addr1.Hex() == addr2.Hex() {
+			continue
+		}
+	}
+}
+
+func BenchmarkTestCompare4(b *testing.B) {
+	addr1 := common.HexToAddress("0x0000000000fC95fD88A4c46d9d7984A56289c52A")
+	addr2 := common.HexToAddress("0x0000000000fc95fd88a4c46d9d7984a56289c52a")
+
+	for i := 0; i < b.N; i++ {
+		if string(addr1[:]) == string(addr2[:]) {
+			continue
+		}
+	}
+}
+
+func TestRoundNWei(t *testing.T) {
+
+	u := Utils{}
+	v := u.ToWei(0.073229374492)
+	fmt.Printf(" %v\n", v)
+	ret, err := u.RoundNWei(v, 5)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("ret %v\n", ret)
+}
