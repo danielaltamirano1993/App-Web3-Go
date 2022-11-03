c8cf3a092f4f0ef361bae82093602420637f8129
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-11-03 09:38:30 -0500

    Create call.go

---
+++ types/call.go
@@ -0,0 +1,43 @@
+package types
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+type CallMsgData []byte
+
+func (a CallMsgData) MarshalText() ([]byte, error) {
+	return hexutil.Bytes(a[:]).MarshalText()
+}
+
+type CallMsgBigInt big.Int
+
+func (a CallMsgBigInt) MarshalText() ([]byte, error) {
+	b := big.Int(a)
+	return []byte(hexutil.EncodeBig(&b)), nil
+}
+
+func NewCallMsgBigInt(v *big.Int) CallMsgBigInt {
+	if v == nil {
+		return CallMsgBigInt(*big.NewInt(0))
+	}
+	i := *v
+	return CallMsgBigInt(i)
+}
+
+type CallMsg struct {
+	From     common.Address `json:"from,omitempty"`
+	To       common.Address `json:"to"`
+	Data     CallMsgData    `json:"data"`
+	GasPrice CallMsgBigInt  `json:"gasPrice,omitempty"`
+	Value    CallMsgBigInt  `json:"value,omitempty"`
+}
+
+type ZeroValueCallMsg struct {
+	From common.Address `json:"from,omitempty"`
+	To   common.Address `json:"to"`
+	Data CallMsgData    `json:"data"`
+}
