04725939ab0e984ba996491b98458789959259e1
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-11-04 09:39:08 -0500

    Create client.go

---
+++ rpc/client.go
@@ -0,0 +1,30 @@
+package rpc
+
+import (
+	"github.com/chenzhijie/go-web3/rpc/transport"
+)
+
+type Client struct {
+	transport transport.Transport
+}
+
+func NewClient(addr, proxy string) (*Client, error) {
+	c := &Client{
+		addr: addr,
+	}
+
+	t, err := transport.NewTransport(addr, proxy)
+	if err != nil {
+		return nil, err
+	}
+	c.transport = t
+	return c, nil
+}
+
+func (c *Client) Close() error {
+	return c.transport.Close()
+}
+
+func (c *Client) Call(method string, out interface{}, params ...interface{}) error {
+	return c.transport.Call(method, out, params...)
+}
